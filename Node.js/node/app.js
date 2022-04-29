ba096f7f6a6b037386c74317004b6a38a1a6ea1f
DavidRivera5 <98625201+DavidRivera5@users.noreply.github.com>
2022-04-29 12:08:22 -0600

    Add files via upload

---
+++ Node.js/node/app.js
@@ -0,0 +1,7 @@
+const {sum,pets } = require('./functions.js')
+
+console.log(`Resultado de suma: ${operations.sum(5,6)}`);
+
+pets.forEach((pet)=>{
+    console.log(pet);
+});
---
+++ Node.js/node/functions.js
@@ -0,0 +1,8 @@
+exports.sum = (a,b) =>a+b;
+exports.pets = ['michi','firulay','cenizas'];
+
+/*module.exports = {
+    Sum
+}*/
+
+//console.log("PROCESS: ", process);
